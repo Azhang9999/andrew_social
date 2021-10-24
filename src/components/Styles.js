d51587b75264a16a34989fdebfdfa04c18150052
Andrew Zhang <and.zhang9.8@gmail.com>
2021-10-24 13:18:15 -0700

    Styles.js added

---
+++ src/components/Styles.js
@@ -0,0 +1,17 @@
+import {makeStyles} from '@mui/styles';
+
+const styles = makeStyles((_: Theme) => ({
+    container: {
+        paddingTop: 50,
+        paddingBottom: 50
+    },
+    submitButton: {
+        paddingLeft:7,
+        paddingRight:7
+    },
+    card: {
+        elevation:6,
+    },
+}))
+
+export default styles
